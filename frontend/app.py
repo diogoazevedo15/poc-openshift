0b93f8c882652af32353352210c80e64823c5281
diogoazevedo15 <68329635+diogoazevedo15@users.noreply.github.com>
2025-02-27 09:24:07 +0000

    Add fronted code, requirements and Dockerfile

---
+++ frontend/app.py
@@ -0,0 +1,28 @@
+import requests
+from flask import Flask, render_template_string
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    try:
+        # URL uses backend’s service name in the cluster: "backend-service" (we'll define it later)
+        response = requests.get('http://backend-service:5000/data', timeout=2)
+        data = response.json()
+        message = data.get('message', 'No message found')
+    except Exception as e:
+        message = f"Could not reach backend. Error: {e}"
+
+    html = f"""
+    <html>
+    <head><title>Frontend</title></head>
+    <body>
+      <h1>Frontend</h1>
+      <p>Backend says: <strong>{message}</strong></p>
+    </body>
+    </html>
+    """
+    return render_template_string(html)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
