5def3bc608a3a12577aff803638912d133960d5e
diogoazevedo15 <68329635+diogoazevedo15@users.noreply.github.com>
2025-03-03 14:21:01 +0000

    Add deploymeny files (#4)

--- backend/app.py
+++ backend/app.py
@@ -7,5 +7,4 @@ def get_data():
     return jsonify({"message": "Hello from the backend!"})
 
 if __name__ == '__main__':
-    # Listen on all interfaces (0.0.0.0) so it's accessible inside the container.
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8010)
